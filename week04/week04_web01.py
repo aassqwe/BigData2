030a39b2e3cb595d4fe6e88f66bd6779f0edf42f
ysy3490 <ysy3490@naver.com>
2024-09-19 10:29:15 +0900

    v1.0 BP2

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -7,9 +7,7 @@ station_id = input("지역코드 : ")
 values = {'stnId':station_id}
 
 url = api + '?' + urllib.parse.urlencode(values)
-print(url)
+# print(url)
 
-"""
-지역코드 : 105
-https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=105
-"""
+urls = urllib.request.urlopen(url).read()
+print(urls)
