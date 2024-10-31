f29f205f48de1b9145b9eabf8d66b76c77da56ea
ysy3490 <ysy3490@naver.com>
2024-10-31 12:37:05 +0900

    v2.6 BP2

--- week10/week10_dataset.py
+++ week10/week10_dataset.py
@@ -1,10 +1,19 @@
 import seaborn as sns
 import pandas as pd
 
+from afd import tbody
+
 titanic = sns.load_dataset('titanic')
 # print(titanic.describe())
 # print(titanic.head())
 # print(len(titanic))
 # print(titanic.tail(10))
 # print(titanic.info())
-print(titanic['embarked'], titanic['deck'], titanic['class'])
+# print(titanic['embarked'], titanic['deck'], titanic['class'])
+
+# print(titanic['deck'])
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
+# print(titanic['deck'])
+titanic['deck'].fillna('Unknown', inplace=True)
+# print(titanic['deck'])
+print(titanic.info())
