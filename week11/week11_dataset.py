5ac14b8950f6d9bf37a793fee2a789dca855d476
ysy3490 <ysy3490@naver.com>
2024-11-07 10:52:00 +0900

    v2.8 BP2

--- week11/week11_dataset.py
+++ week11/week11_dataset.py
@@ -4,14 +4,21 @@ import matplotlib.pylab as plt
 
 
 titanic = sns.load_dataset('titanic')
+# print(titanic['sex'].head())
+# print(titanic.info())
+gemder_survival = titanic.groupby(by='sex')['survived'].mean()
+print(gemder_survival)
+
+
+
 # titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
 # titanic['deck'].fillna('Unknown', inplace=True)
 # print(titanic['deck'])
 # print(titanic.info())
-print(titanic['survived'])
-print(titanic.info())
-sns.countplot(data=titanic, x='survived')
-plt.title('Survived (0 = No, 1 = Yes)')
-plt.xlabel('Survived')
-plt.ylabel('Count')
-plt.show()
+# print(titanic['survived'])
+# print(titanic.info())
+# sns.countplot(data=titanic, x='survived')
+# plt.title('Survived (0 = No, 1 = Yes)')
+# plt.xlabel('Survived')
+# plt.ylabel('Count')
+# plt.show()
