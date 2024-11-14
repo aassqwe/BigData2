7c556af5dfb69b8764520aa57f35af885a413da8
ysy3490 <ysy3490@naver.com>
2024-11-14 10:53:00 +0900

    v3.1 BP2

---
+++ week12/week12_dataset.py
@@ -0,0 +1,31 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pandas.core.common import random_state
+from  sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+# 나이에 따른 생존율 계산
+titanic = sns.load_dataset('titanic')
+median_age = titanic['age'].median()
+titanic_fill_row = titanic.fillna({'age' : median_age})
+
+X = titanic_fill_row[['age']]
+y = titanic_fill_row['survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print(y_pred)
+print(y_test)
+
+plt.figure(figsize=(3, 2))
+plt.scatter(X_test, y_test, color='blue', label='Actual')
+plt.scatter(X_test, y_pred, color='red', label='Predicted')
+plt.title('Linear Regression: Real vs predicted')
+plt.xlabel('Age')
+plt.ylabel('Survivied')
+plt.show()
+
