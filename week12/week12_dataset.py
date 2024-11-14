b6c73aef7943a76e38dd88a88f081a4fdbadf9b8
ysy3490 <ysy3490@naver.com>
2024-11-14 12:08:36 +0900

    v0.1 BP2 LogisticRegression

--- week12/week12_dataset.py
+++ week12/week12_dataset.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
@@ -14,16 +14,16 @@ y = titanic_fill_row['survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = KNeighborsRegressor(n_neighbors=5)
+model = LogisticRegression()
+
 model.fit(X_train, y_train)
+
 y_pred = model.predict(X_test)
-print(y_pred)
-print(y_test)
 
 plt.figure(figsize=(5, 2))
 plt.scatter(X_test, y_test, color='blue', label='Actual')
-plt.scatter(X_test, y_pred, color='red', label='Predicted')
-plt.title('Linear Regression: Real vs predicted')
+plt.scatter(X_test, y_pred, color='red', label='Predicted', alpha=0.3)
+plt.title('Logistic Regression: Real vs predicted')
 plt.xlabel('Age')
 plt.ylabel('Survivied')
 plt.show()
