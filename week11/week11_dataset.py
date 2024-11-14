e1d704a4cf0828b2f2158c606152b74d8e0391ab
ysy3490 <ysy3490@naver.com>
2024-11-14 11:11:17 +0900

    v3.2 BP2

--- week11/week11_dataset.py
+++ week11/week11_dataset.py
@@ -5,6 +5,16 @@ import matplotlib.pylab as plt
 
 titanic = sns.load_dataset('titanic')
 
+median_age = titanic['age'].median()
+titanic_fill_row = titanic.fillna({'age' : median_age})
+titanic_fill_row['survived'] = titanic_fill_row['survived'].astype(float)
+plt.figure(figsize=(10, 5))
+sns.histplot(data=titanic_fill_row, x='age', weights='survived', bins=20, kde=False)
+plt.title('Survival Rate bt Age (fill row)')
+plt.xlabel('Age')
+plt.ylabel('survival Rate (weights)')
+plt.show()
+
 titanic_drop_row = titanic.dropna(subset=['age'])
 titanic_drop_row['survived'] = titanic_drop_row['survived'].astype(float)
 plt.figure(figsize=(10, 5))
@@ -13,6 +23,7 @@ plt.title('Survival Rate bt Age (Drop row)')
 plt.xlabel('Age')
 plt.ylabel('survival Rate (weights)')
 plt.show()
+
 # print(titanic['sex'].head())
 # print(titanic.info())
 # gender_survival = titanic.groupby(by='sex')['survived'].mean()
@@ -20,6 +31,7 @@ plt.show()
 # gender_survival = titanic.groupby(by='sex')['survived'].mean().reset_index()
 # print(type(gender_survival))
 # print(titanic.info())
+
 # sns.barplot(data=gender_survival, x='sex', y='survived')
 # plt.title('Survival Rate by Gender')
 # plt.ylabel('Survival Rate')
