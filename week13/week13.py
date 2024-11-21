b5213c94004f132e0742b0f9736a319645705a63
ysy3490 <ysy3490@naver.com>
2024-11-21 11:57:25 +0900

    v3.9 BP2

--- week13/week13.py
+++ week13/week13.py
@@ -1,15 +1,35 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 mpg = sns.load_dataset('mpg')
 mpg.drop(['name'], axis=1, inplace=True)
 mpg.dropna(inplace=True)
 mpg = pd.get_dummies(mpg, columns=['origin'], drop_first=True)
-# print(mpg.info())
 
 X = mpg.drop(['mpg'], axis=1)
 y = mpg['mpg']
 
-print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squard Error : {mse}")
+print(f"R2 Score : {r2}")
+
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, color='blue', alpha=0.6)
+plt.plot([y.min(), y.max()], [y.min(), y.max()], color='red', linestyle='--')
+plt.title('Actual MPG vs Predicted MPG')
+plt.xlabel('Actual MPG')
+plt.ylabel('Predicted MPG')
+plt.grid(True)
+plt.show()
